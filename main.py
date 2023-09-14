386348c2cc5edc3b6991373b66decd874237ecdd
josephinenakanjako <143077466+josephinenakanjako@users.noreply.github.com>
2023-09-14 06:01:04 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#change 80 to 89 in the list
+student_marks=[60,80,90,98]
+student_marks[1]=89
+print(student_marks)
+
+#add a new list 55(append a new list)
+student_marks=[60,80,90,98]
+student_marks.append(55)
+print(student_marks)
+
+#find the size of the list having appended 55
+print(len(student_marks))
+
+#write a python program to sum all the items in the list
+total=sum(student_marks)
+print(total)
+
+#write apython function that takes two lists and returns ,if they have at least one common member
+j=input("Enter items")
+k=input("Enter items")
+print("yes,there is a common member in jand k")
+    
+
